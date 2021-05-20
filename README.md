e426fab736cad9e98becc5ced44347eedf581e3f
fortin <fortin@ttu.ee>
2021-05-20 10:27:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+·# Simple PDF merger
+> CLI app for merging several pdf files
+
+There many ways to merge pdf's, some of them payed and some of them free online, but you don't know what those online options do with your files. Here is a simple script to merge several pdf's using the CLI.
+## Requirements
+Python
+
+## USAGE
+Just clone this repository in the folder where you have the files. Or just give the full path to the pdf files you want to merge. After the script name type the file names (paths). If not especified the output file is ```merge.pdf```
+```bash
+python mergepdf.py files_paths --output=file_path
+```
+
+## EXAMPLE
+```bash
+python mergepdf.py document1.pdf document1.pdf --ouput=merged.pdf
+```
+
+## CODE
+The core is taken from the [Real Python Tutorial](https://realpython.com/pdf-python/#how-to-merge-pdfs)
+I just added the CLI part
---
+++ mergepdf.py
@@ -0,0 +1,44 @@
+import sys
+from sys import argv
+
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+if not sys.warnoptions:
+    import warnings
+    warnings.simplefilter("ignore")
+
+
+def merge_pdfs(paths, output):
+    pdf_writer = PdfFileWriter()
+
+    for path in paths:
+        pdf_reader = PdfFileReader(path)
+        for page in range(pdf_reader.getNumPages()):
+            # Add each page to the writer object
+            pdf_writer.addPage(pdf_reader.getPage(page))
+
+    # Write out the merged PDF
+    with open(output, 'wb') as out:
+        pdf_writer.write(out)
+
+
+if __name__ == '__main__':
+    paths = []
+    output_file = 'merged.pdf'
+    for i in range(1, len(argv)):
+        if '--output=' in argv[i]:
+            output_file = argv[i].split('=')[-1]
+        elif argv[i] == '-h' or argv[i] == '--help':
+            print('Usage: python mergepdf.py [file_paths] \[--output=output_file_path]\
+                \n\t file_paths\tFiles to merge paths (full or relative)\
+                \n\t --output=\toutput file name\
+                \n\t -h --help\tshow this help\
+                \nOrder of the given files will be the order they are merged')
+        else:
+            paths.append(argv[i])
+    try:
+        merge_pdfs(paths, output=output_file)
+        print('Succesfully merged to file: {}'.format(output_file))
+    except:
+        print('\n\tPlease, check the input parameters\
+            \n\n\tUsage help: python mergepdf.py -h')
